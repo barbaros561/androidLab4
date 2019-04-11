85035e1843507f934049f1210e718d0ff4d41a29
barbaros561 <k.kondracki561@gmail.com>
2019-04-11 12:42:58 +0200

    obsługa formularza

--- app/src/main/java/com/example/androidlab4/MainActivity.java
+++ app/src/main/java/com/example/androidlab4/MainActivity.java
@@ -41,13 +41,23 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
-    public void nowyWpis(MenuItem mi)
-    {
+
+    public void nowyWpis(MenuItem mi) {
         Intent intencja = new Intent(this,
                 DodajWpis.class);
         startActivityForResult(intencja, 1);
     }
 
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            String nowy = (String) extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
 
+    }
 }
 
