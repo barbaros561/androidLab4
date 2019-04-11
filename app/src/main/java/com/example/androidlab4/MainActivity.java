c9f0eb767a67cf1c3f9eea7412e2eda4c4dbfef8
barbaros561 <k.kondracki561@gmail.com>
2019-04-11 12:41:26 +0200

    tworzenie formularza

--- app/src/main/java/com/example/androidlab4/MainActivity.java
+++ app/src/main/java/com/example/androidlab4/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.androidlab4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,9 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> target;
     private ArrayAdapter adapter;
-    String[] values = new String[] { "Pies",
+    String[] values = new String[]{"Pies",
             "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp",
-            "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch" };
+            "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch"};
 
 
     @Override
@@ -29,7 +33,21 @@ public class MainActivity extends AppCompatActivity {
         listview.setAdapter(this.adapter);
 
 
-
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this,
+                DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
+
 }
+
